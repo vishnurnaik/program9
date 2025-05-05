260eb278d3f287a2235a3bbfdea4d35a355c55d1
shashank01 <shashank018@gmail.com>
2025-05-05 11:52:33 +0530

    azure pipeline example

---
+++ cse/src/main/java/com/1cd22cs418/App.java
@@ -0,0 +1,13 @@
+package com.1cd22cs418;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
